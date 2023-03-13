56ce986c004b5b60c1b1ff1541c9812987be33a9
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2023-03-13 14:57:17 -0300

    Aula-10/03

---
+++ app/src/main/java/com/ifsc/aula10_03/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ifsc.aula10_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextTextPersonName);
+        Log.d("metodocallback","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("metodocallback","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("metodocallback","onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("metodocallback","onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("metodocallback","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("metodocallback","onDestroy");
+    }
+    public void iniciaActivity(View view){
+        Intent i = new Intent(getApplicationContext(), imc.class);
+        String mensagem = editText.getText().toString();
+        i.putExtra("msg",mensagem);
+        startActivity(i);
+
+    }
+
+}
