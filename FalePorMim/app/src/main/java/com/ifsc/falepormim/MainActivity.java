5c4e5ca441a171390082d1c797ae682511782304
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2023-03-30 15:26:44 -0300

    Fale por mim

---
+++ FalePorMim/app/src/main/java/com/ifsc/falepormim/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ifsc.falepormim;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etTextInput;
+    private Button btnSpeak;
+    private TextToSpeechHelper textToSpeechHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etTextInput = findViewById(R.id.editText);
+        btnSpeak = findViewById(R.id.buttonSpeak);
+
+        textToSpeechHelper = new TextToSpeechHelper(this);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = etTextInput.getText().toString();
+                if (!text.isEmpty()) {
+                    textToSpeechHelper.speak(text);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textToSpeechHelper.release();
+    }
+}
---
+++ FalePorMim/app/src/main/java/com/ifsc/falepormim/TextToSpeechHelper.java
@@ -0,0 +1,29 @@
+package com.ifsc.falepormim;
+
+
+import android.content.Context;
+        import android.speech.tts.TextToSpeech;
+        import java.util.Locale;
+
+public class TextToSpeechHelper implements TextToSpeech.OnInitListener {
+    private TextToSpeech tts;
+    private boolean isReady = false;
+
+    public TextToSpeechHelper(Context context) {
+        tts = new TextToSpeech(context, this);
+    }
+
+    public void speak(String text) {
+        if (isReady) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.getDefault());
+            isReady = true;
+        }
+    }
+}
