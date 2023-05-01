6fea8dc66e767c669f7e261bd47d7e4f3a1ba169
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2023-05-01 11:29:15 -0300

    My Laucher
    Atividade pronta

--- MyLaucher2/app/src/main/java/br/com/ifsc/mylaucher/MainActivity.java
+++ MyLaucher2/app/src/main/java/br/com/ifsc/mylaucher/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.List;
@@ -15,8 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ListView listView = findViewById(R.id.ListView);
         packageManager= getPackageManager();
         List<ApplicationInfo> packageInfoList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        ArrayAdapter appAdapter = new AppAdapter(this,R.layout.item_lista,packageInfoList);
+        listView.setAdapter(appAdapter);
     }
 }
